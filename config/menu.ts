43d77a4af63993d5b6f43c03a8edd0d7fea87058
eveningwater <854806732@qq.com>
2022-07-11 13:37:32 +0800

    添加了新的react

--- config/menu.ts
+++ config/menu.ts
@@ -8,6 +8,10 @@ export default {
       title: 'Alert',
       path: '/guide/Alert/Alert',
     },
+    {
+      title: 'AutoLink',
+      path: '/guide/AutoLink/AutoLink',
+    },
   ],
   '/zh-CN/guide': [
     {
@@ -17,6 +21,10 @@ export default {
     {
       title: '提示',
       path: '/zh-CN/guide/Alert/Alert',
+    },
+    {
+      title: '自动文本链接',
+      path: '/zh-CN/guide/AutoLink/AutoLink',
     },
   ],
 };
